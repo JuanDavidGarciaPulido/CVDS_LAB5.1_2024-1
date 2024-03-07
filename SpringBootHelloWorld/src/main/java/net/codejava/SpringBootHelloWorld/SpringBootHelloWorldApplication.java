98ff8aa8c84712df7313fcca4ac6218f9baa758a
Juan David García Pulido <juan.david.garcia.pulido@gmail.com>
2024-03-07 10:27:11 -0500

    Parte 3

--- SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
+++ SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @RestController
@@ -13,12 +14,13 @@ public class SpringBootHelloWorldApplication {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
 
-
+	@CrossOrigin("http://localhost:3000")
 	@RequestMapping("/")
 	public String home(){
 		return "<h1>Hello world Spring Boot</h1><p>Welcome to my Spring Boot application!</p>";
 	}
 
+	@CrossOrigin("http://localhost:3000")
 	@RequestMapping("/new")
 	public String newMessage(){
 		return "LAB 5.1: Aplicación";
