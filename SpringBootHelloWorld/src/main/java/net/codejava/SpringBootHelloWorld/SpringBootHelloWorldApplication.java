c7cb26e5c162152ad39230c4fd45b29bc88c13c8
Juan David García Pulido <juan.david.garcia.pulido@gmail.com>
2024-03-06 20:14:23 -0500

    Parte 1

---
+++ SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -0,0 +1,26 @@
+package net.codejava.SpringBootHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+
+
+	@RequestMapping("/")
+	public String home(){
+		return "<h1>Hello world Spring Boot</h1><p>Welcome to my Spring Boot application!</p>";
+	}
+
+	@RequestMapping("/new")
+	public String newMessage(){
+		return "LAB 5.1: Aplicación";
+	}
+}
